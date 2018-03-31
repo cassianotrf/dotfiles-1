103a4c416b9b668c45b328d63d5f98507241f655
Rafael Moreira <rafael@rafaelmoreira.org>
2018-03-31 10:32:46 -0300

    add swap to hyper-v

--- bootstrap/arch/sys-install/pre-chroot-install.sh
+++ bootstrap/arch/sys-install/pre-chroot-install.sh
@@ -20,6 +20,8 @@ elif [ $mtype == "hyper-v" ]
 then
 	mkfs.fat -F32 /dev/sda1
 	mkfs.ext4 /dev/sda2
+	mkswap /dev/sda3
+	swapon /dev/sda3
 	mount /dev/sda2 /mnt
 	mkdir /mnt/boot
 	mount /dev/sda1 /mnt/boot
